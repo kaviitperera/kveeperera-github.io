76b1abd07afc4be193ece77aac87e70380813f68
kaviitperera <115469827+kaviitperera@users.noreply.github.com>
2022-10-10 20:54:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const Home= () => {
+    window.location.href = 'Index.html';
+}
+
+const Contactme= () => {
+    window.location.href = 'Gallery.html';
+}
+
+const Aboutme= () => {
+    window.location.href = 'About.html';
+}
+
+const Education= () => {
+    window.location.href = 'Achievements.html';
+}
+
